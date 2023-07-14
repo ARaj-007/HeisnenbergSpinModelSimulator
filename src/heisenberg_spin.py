02186ff34d85395dd8294defbad0e165bcd5796f
Abhiyansh Raj <abhiyansh2002@gmail.com>
2023-07-14 23:34:51 +0530

    Added functions to calculate the eigenvalue and eigenvectors

--- src/heisenberg_spin.py
+++ src/heisenberg_spin.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 
 def heisenberg_hamiltonian():
     sx = np.array([[0, 1], [1, 0]])
@@ -6,3 +6,13 @@ def heisenberg_hamiltonian():
     sz = np.array([[1, 0], [0, -1]])
     h = np.kron(sx, sx) + np.kron(sy, sy) + np.kron(sz, sz)
     return h
+
+def calculate_eigenvalues(matrix):
+    eigenvalues, _ = np.linalg.eig(matrix)
+    return eigenvalues
+
+def calculate_eigenvectors(matrix):
+    _, eigenvectors = np.linalg.eig(matrix)
+    return eigenvectors
+
+# Additional utility functions or classes related to the Heisenberg spin model
